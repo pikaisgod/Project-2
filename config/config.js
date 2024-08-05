96dd250aecdedeacc1b55974790efd85e2a7e4c0
pikaisgod <139665711+pikaisgod@users.noreply.github.com>
2024-08-05 18:02:34 -0400

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,23 +1,21 @@
 require('dotenv').config();
 
-console.log('Database Configuration:', {
+module.exports = {
+  development: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT
-});
-
-module.exports = {
-    development: {
-        username: process.env.DB_USER || '',
-        password: process.env.DB_PASSWORD || '',
-        database: process.env.DB_NAME || '',
-        host: process.env.DB_HOST || '127.0.0.1',
-        dialect: process.env.DB_DIALECT || 'postgres'
-    },
-    production: {
-        use_env_variable: 'DATABASE_URL',
-        dialect: 'postgres'
+    dialect: process.env.DB_DIALECT,
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
     }
+  },
 };
